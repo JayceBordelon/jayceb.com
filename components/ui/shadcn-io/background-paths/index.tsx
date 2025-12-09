eacef96c868cb1a77fc0b60b7a7f576f38c58af9
Jayce Bordelon <b.jayce@wustl.edu>
2025-12-08 18:25:48 -0600

    Stretch

--- components/ui/shadcn-io/background-paths/index.tsx
+++ components/ui/shadcn-io/background-paths/index.tsx
@@ -4,22 +4,26 @@
 import { motion } from 'framer-motion';
 
 function FloatingPaths({ position }: { position: number }) {
+  const spread = 1.6;
+
   const paths = Array.from({ length: 75 }, (_, i) => ({
     id: i,
-    d: `M-${600 - i * 12 * position} -${189 + i * 10}C-${
-      600 - i * 12 * position
-    } -${189 + i * 10} -${500 - i * 12 * position} ${216 - i * 10} ${
-      300 - i * 12 * position
-    } ${343 - i * 10}C${900 - i * 12 * position} ${470 - i * 10} ${
-      1000 - i * 12 * position
-    } ${875 - i * 10} ${1000 - i * 12 * position} ${875 - i * 10}`,
+    d: `M-${600 * spread - i * 12 * position} -${189 + i * 10}C-${
+      600 * spread - i * 12 * position
+    } -${189 + i * 10} -${500 * spread - i * 12 * position} ${
+      216 - i * 10
+    } ${300 * spread - i * 12 * position} ${343 - i * 10}C${
+      900 * spread - i * 12 * position
+    } ${470 - i * 10} ${1000 * spread - i * 12 * position} ${875 - i * 10} ${
+      1000 * spread - i * 12 * position
+    } ${875 - i * 10}`,
     width: 0.5 + i * 0.03,
   }));
 
   return (
     <svg
       className="w-full h-full text-primary"
-      viewBox="-800 -600 3000 1400"
+      viewBox={`${-1000 * spread} -800 ${3000 * spread} 1400`}
       fill="none"
       preserveAspectRatio="xMidYMid slice"
     >
